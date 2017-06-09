7f81f4632e95ae9f73369c9c437c3d098bf3a386
balabalabu <lovesjer@126.com>
2017-06-09 11:02:13 +0800

    Add files via upload

---
+++ imooc/school/klass.js
@@ -0,0 +1,16 @@
+var student = require('./student');
+var teacher = require('./teacher');
+
+teacher.add('Scott');
+
+function add (teacherName,students) {
+	teacher;
+
+	students.forEach( function(item, index) {
+		student.add(item);
+	});
+}
+
+exports.add = add;
+
+//module.exports = add
